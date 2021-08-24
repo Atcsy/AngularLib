c1b8faa7792209619f1cb5aac2075881f829bc7b
katona.dev@gmail.com <katona.dev@gmail.com>
2021-08-24 19:26:14 +0200

    added cart folder structure

--- src/app/library/library.service.ts
+++ src/app/library/library.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable, Input } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 import { IBook } from '../shared/models/book';
 import { IPagination } from '../shared/models/pagination';
@@ -9,7 +10,7 @@ import { IPagination } from '../shared/models/pagination';
   providedIn: 'root',
 })
 export class LibraryService {
-  baseUrl = 'http://localhost:5000/api/v1/';
+  baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
